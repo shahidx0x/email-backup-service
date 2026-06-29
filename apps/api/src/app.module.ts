48a7cff617f8aa1e1db1e8e30d20a68713ce992f
Abdulla Al Shahid <shahid.dev.cse@gmail.com>
2026-06-29 15:14:55 +0600

    feat: register export and restore modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -12,6 +12,7 @@ import { InfrastructureModule } from './infrastructure';
 import { MailboxesModule } from './mailboxes';
 import { MessagesModule } from './messages';
 import { JobsModule } from './jobs';
+import { TransfersModule } from './transfers';
 
 @Module({
  imports:[
@@ -19,7 +20,7 @@ import { JobsModule } from './jobs';
   LoggerModule.forRootAsync({inject:[ConfigService],useFactory:(c:ConfigService<AppConfig,true>)=>createLoggerConfig(c.get('LOG_LEVEL',{infer:true}))}),
   MongooseModule.forRootAsync({inject:[ConfigService],useFactory:(c:ConfigService<AppConfig,true>)=>({uri:buildMongoUri(parseConfig(process.env)),autoIndex:c.get('NODE_ENV',{infer:true})!=='production',serverSelectionTimeoutMS:10_000})}),
   ThrottlerModule.forRoot([{ttl:60_000,limit:120}]),
-  AuthModule,InfrastructureModule,MailboxesModule,MessagesModule,JobsModule,
+  AuthModule,InfrastructureModule,MailboxesModule,MessagesModule,JobsModule,TransfersModule,
  ],
  providers:[{provide:APP_GUARD,useClass:JwtAuthGuard},{provide:APP_GUARD,useClass:RolesGuard},{provide:APP_FILTER,useClass:ApiExceptionFilter}],
 })
